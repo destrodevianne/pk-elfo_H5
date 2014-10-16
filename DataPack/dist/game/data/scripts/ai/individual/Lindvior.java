3fb4cd66c69e687efb61735341c6b5b3ba5fc023
PkElfo <PkElfo@13720c4f-9a1f-4619-977f-b36a0ac534ba>
2014-10-16 01:39:05 +0000

    Fix skills de augment  - Might (ativo, passivo e chance)  - Duel Might (ativo, passivo e chance)  - Shield (ativo, passivo e chance)
    Fix Baium
    Fix Lindvior.java

--- DataPack/dist/game/data/scripts/ai/individual/Lindvior.java
+++ DataPack/dist/game/data/scripts/ai/individual/Lindvior.java
@@ -1,21 +1,3 @@
-/*
- * Copyright (C) 2004-2013 L2J DataPack
- * 
- * This file is part of L2J DataPack.
- * 
- * L2J DataPack is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * L2J DataPack is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 package ai.individual;
 
 import java.util.Calendar;
@@ -28,10 +10,6 @@ import pk.elfo.gameserver.network.NpcStringId;
 import pk.elfo.gameserver.network.clientpackets.Say2;
 import ai.npc.AbstractNpcAI;
 
-/**
- * Lindvior Scene AI.
- * @author nonom
- */
 public class Lindvior extends AbstractNpcAI
 {
 	private static final int LINDVIOR_CAMERA = 18669;
@@ -54,7 +32,7 @@ public class Lindvior extends AbstractNpcAI
 	
 	private Lindvior()
 	{
-		super(Lindvior.class.getSimpleName(), "ai/individual/");
+		super(Lindvior.class.getSimpleName(), "ai/individual");
 		scheduleNextLindviorVisit();
 	}
 	
