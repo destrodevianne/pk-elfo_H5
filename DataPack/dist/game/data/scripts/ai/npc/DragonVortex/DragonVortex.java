4b84604b7ed34587a183d6eedf1a7c74a8b9069b
PkElfo <PkElfo@13720c4f-9a1f-4619-977f-b36a0ac534ba>
2014-11-01 06:01:31 +0000

    Dragon Vortex - Retail Like Call Monsters {Show effects when Spawn}..

--- DataPack/dist/game/data/scripts/ai/npc/DragonVortex/DragonVortex.java
+++ DataPack/dist/game/data/scripts/ai/npc/DragonVortex/DragonVortex.java
@@ -1,19 +1,17 @@
 package ai.npc.DragonVortex;
 
-import pk.elfo.gameserver.model.Location;
-import pk.elfo.gameserver.model.actor.L2Npc;
-import pk.elfo.gameserver.model.actor.instance.L2PcInstance;
 import ai.npc.AbstractNpcAI;
 
-/**
- * PkElfo
- */
-public class DragonVortex extends AbstractNpcAI
+import pk.elfo.gameserver.model.actor.L2Npc;
+import pk.elfo.gameserver.model.actor.instance.L2PcInstance;
+
+public final class DragonVortex extends AbstractNpcAI
 {
-	private static final int VORTEX = 32871;
-	
-	private static final int[] RAIDS =
-	{
+    // NPC
+    private static final int VORTEX = 32871;
+    // Raids
+    private static final int[] RAIDS =
+    {
         25718, // Emerald Horn
         25719, // Dust Rider
         25720, // Bleeding Fly
@@ -21,44 +19,70 @@ public class DragonVortex extends AbstractNpcAI
         25722, // Shadow Summoner
         25723, // Spike Slasher
         25724, // Muscle Bomber
-	};
-	
-	private static final int LARGE_DRAGON_BONE = 17248;
-	
-	private static final int DESPAWN_DELAY = 1800000; // 30min
-	
-	private DragonVortex(String name, String descr)
-	{
-		super(name, descr);
-		addStartNpc(VORTEX);
-		addFirstTalkId(VORTEX);
-		addTalkId(VORTEX);
-		addKillId(RAIDS);
-	}
-	
-	@Override
-	public String onAdvEvent(String event, L2Npc npc, L2PcInstance player)
-	{
-		if ("Spawn".equals(event))
-		{
-			int raid = RAIDS[getRandom(RAIDS.length)];
-			
-			if (hasQuestItems(player, LARGE_DRAGON_BONE))
-			{
-				takeItems(player, LARGE_DRAGON_BONE, 1);
-				addSpawn(raid, new Location(player.getX() + getRandom(400), player.getY() + getRandom(400), player.getZ(), player.getHeading()), true, DESPAWN_DELAY);
-			}
-			else
-			{
-				return "32871-no.html";
-			}
-		}
-		
-		return super.onAdvEvent(event, npc, player);
-	}
-	
-	public static void main(String[] args)
-	{
-		new DragonVortex(DragonVortex.class.getSimpleName(), "ai/npc/");
-	}
+    };
+    // Item
+    private static final int LARGE_DRAGON_BONE = 17248;
+    // Misc
+    private static final int DESPAWN_DELAY = 1800000; // 30min
+
+    private DragonVortex()
+    {
+        super(DragonVortex.class.getSimpleName(), "ai/npc");
+        addStartNpc(VORTEX);
+        addFirstTalkId(VORTEX);
+        addTalkId(VORTEX);
+    }
+
+    @Override
+    public String onAdvEvent(String event, L2Npc npc, L2PcInstance player)
+    {
+        if ("Spawn".equals(event))
+        {
+            if (hasQuestItems(player, LARGE_DRAGON_BONE))
+            {
+                takeItems(player, LARGE_DRAGON_BONE, 1);
+                final int random = getRandom(1000);
+                int raid = 0;
+                if (random < 292)
+                {
+                    raid = RAIDS[0]; // Emerald Horn 29.2%
+                }
+                else if (random < 516)
+                {
+                    raid = RAIDS[1]; // Dust Rider 22.4%
+                }
+                else if (random < 692)
+                {
+                    raid = RAIDS[2]; // Bleeding Fly 17.6%
+                }
+                else if (random < 808)
+                {
+                    raid = RAIDS[3]; // Blackdagger Wing 11.6%
+                }
+                else if (random < 900)
+                {
+                    raid = RAIDS[4]; // Spike Slasher 9.2%
+                }
+                else if (random < 956)
+                {
+                    raid = RAIDS[5]; // Shadow Summoner 5.6%
+                }
+                else
+                {
+                    raid = RAIDS[6]; // Muscle Bomber 4.4%
+                }
+                addSpawn(raid, npc.getX() + getRandom(-500, 500), npc.getY() + getRandom(-500, 500), npc.getZ() + 10, 0, false, DESPAWN_DELAY, true);
+            }
+            else
+            {
+                return "32871-no.html";
+            }
+        }
+        return super.onAdvEvent(event, npc, player);
+    }
+
+    public static void main(String[] args)
+    {
+        new DragonVortex();
+    }
 }
